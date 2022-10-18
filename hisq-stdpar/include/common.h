d7c1e0f669f1fee5447380671d5b4ec84d3af8ae
alexstrel <astrel@fnal.gov>
2022-10-18 13:22:18 -0500

    initial commit

--- hisq-stdpar/include/common.h
+++ hisq-stdpar/include/common.h
@@ -3,7 +3,17 @@
 #include <concepts> 
 #include <type_traits>
 
+// Simple arithmetic type
 template <typename T>
-concept ArithmeticTp = std::is_arithmetic_v<T>;
+concept ArithmeticTp = requires{
+  std::is_arithmetic_v<T>;
+}
+
+// Simple complex type
+template <typename T>
+concept ComplexTp    = requires (T t) {
+  requires ArithmeticTp<decltype(t.real())>;
+  requires ArithmeticTp<decltype(t.imag())>;  
+}
 
 
