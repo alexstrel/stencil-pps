0f731d2a3a400e3915aecb411c47b7954d94c7fb
Strelchenko Alexei <astrel@fnal.gov>
2023-02-09 17:56:09 -0600

    initial commit

---
+++ schwinger-stdpar/include/common.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <concepts> 
+#include <type_traits>
+
+
+// Simple arithmetic type
+template <typename T>
+concept FloatTp = requires{
+  std::is_floating_point_v<T>;
+};
+
+// Simple complex type
+template <typename T>
+concept ComplexTp    = requires (T t) {
+  requires FloatTp<decltype(t.real())>;
+  requires FloatTp<decltype(t.imag())>;  
+};
+
+// Simple genetic arithmetic type
+template <typename T>
+concept ArithmeticTp = FloatTp<T> || ComplexTp<T>;
+
