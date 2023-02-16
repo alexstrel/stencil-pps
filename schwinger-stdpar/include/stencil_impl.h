b6f7dc26f0b1c22bafc661488ff9771d00c743e5
Alex Strel <astrel@fnal.gov>
2023-02-16 10:07:28 -0600

    a few changes

--- schwinger-stdpar/include/stencil_impl.h
+++ schwinger-stdpar/include/stencil_impl.h
@@ -13,34 +13,36 @@ void dispatch_dslash_kernel(SpinorField &out, const GaugeField &gauge, const Spi
   auto [even_out, odd_out] = out.EODecompose();
 
   const auto [even_in, odd_in]       = in.EODecompose();
-  const auto [even_gauge, odd_gauge] = in.EODecompose();  
+  const auto [even_gauge, odd_gauge] = gauge.EODecompose();  
 
   // Dslash = (I-\kappa \Sum_{mu} [ (I - \sigma_{\mu})U_(x){\mu} \delta_{x+\mu} + (I + \sigma_{\mu})U^*(x-mu)_{\mu}) \delta_{x-mu}]
   //        = I_{EE}  D_{EO}
   //          D_{OE}  I_{OO}
 						 
   // Create the kernel:
-  auto Deo = [out = out.data(), in = in.data(), args](auto cartesian_coords) {
+  auto Dslash = [&o_e      = even_out->Accessor(), 
+                 &o_o      = odd_out->Accessor(), 
+                 &U_e      = even_gauge->Accessor(), 
+                 &U_o      = odd_gauge->Accessor(), 
+                 &i_e      = even_in->Accessor(), 
+                 &i_o      = odd_in->Accessor()](auto cartesian_coords) {
                           
-                          auto idx = [=](auto x, auto y){ 
-                               return x + y*args.nx;
-                          };
+                          auto [y_, xh_] = cartesian_coords;
                           
-                          auto [y, x] = cartesian_coords;
-                                                               
-                          // Load hopping terms or apply boundary conditions:                         
-                          const auto in_xm1 = x == 1             ? 0 : in[idx(x - 1, y)];
-                          const auto in_xp1 = x == (args.nx - 2) ? 0 : in[idx(x + 1, y)];                          
+                          const auto other_parity_site_forward_hop = y_ & 1; //(y+z+t) & 1 => parity index
+                          const auto other_parity_site_bckward_hop = other_site_forward_hop - 1;                           
+                          
+                          const auto xhp1  = xh_ + other_site_forward_hop;
+                          const auto xhm1  = xh_ + other_site_bckward_hop; 
                           //
-                          const auto in_ym1 = y == 1             ? 1 : in[idx(x, y - 1)];
-                          const auto in_yp1 = y == (args.ny - 2) ? 0 : in[idx(x, y + 1)]; 
-
+                          const auto yp1   = y_ + 1;
+                          const auto ym1   = y_ - 1; 
+                           
                           auto site_stencil = (args.C(0)*in[idx(x, y)] + args.C(1) * (in_ym1 + in_yp1 + in_xm1 + in_xp1));
 
                           //store the result
                           out[idx(x, y)] = site_stencil;
                           // we may want to return something if we plan to run the reduction-like algorithm
-                          return compute_energy ? site_stencil*args.dx*args.dx : 0.0;
                         };
                         
   if constexpr (compute_energy){                          
