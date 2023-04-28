c93aab52bc481b5075461ff36716d8f4aa9bd661
Alex Strel <astrel@fnal.gov>
2023-04-28 14:47:07 -0500

    wip commit

--- schwinger-stdpar/include/core/memory.h
+++ schwinger-stdpar/include/core/memory.h
@@ -3,20 +3,37 @@
 #include <memory>
 #include <memory_resource>
 
-static std::size_t pool_bytes = 0ul;
+static std::pmr::memory_resource *upstream = std::pmr::get_default_resource(); 
 
-static std::shared_ptr<std::byte[]> pool_ptr = nullptr;
+class PMRBuffer{
+  public:
+    //
+    std::shared_ptr<std::byte[]>    pmr_buffer;//needed only for pmr-used spinor
+    std::size_t                     pmr_bytes;
 
-/**
-*      @return mapped memory allocated
-*         
-**/
-//std::size_t allocated();
+    PMRState  pmr_state = PMRState::InvalidState; 
 
-/**
- *     @return is prefetching support enabled
- **/
-//bool is_prefetch_enabled();
+    PMRBuffer(const std::size_t nbytes, const enum state) pmr_bytes(nbytes), pmr_state(state) : {
+      pmr_buffer = std::make_shared<std::byte[]>(pmr_bytes);
+    } 
+
+    PMRBuffer()                             = default;
+    PMRBuffer(const PMRBuffer &)            = default;
+    PMRBuffer(PMRBuffer &&)                 = default;
+
+    virtual ~PMRBuffer()                    = default;
+
+    auto Data()  const { return pmr_buffer;       }
+    auto Get()   const { return pmr_buffer.get(); }
+    auto Byte()  const { return pmr_bytes;        }    
+    auto State() const { return pmr_state;        }        
+    
+    auto SetState(PMRState state) { rpmr_state = state; }            
+
+    PMRBuffer &operator=(const PMRBuffer &) = default;
+    PMRBuffer &operator=(PMRBuffer &&)      = default;
+};
+
 
 
 inline void allocate_pmr_pool(const std::size_t bytes) {
@@ -74,6 +91,151 @@ decltype(auto) get_pmr_container(const size_t n) {
 
   return T(n, &pmr_pool);
 }
+
+/**
+    Derived from the QUDA library, original developer : Kate Clark (NVIDIA)
+*/
+
+
+
+namespace pmr_pool
+{
+  /** Cache of pmr-memory allocations.  We cache pmr
+      memory allocations so that fields can reuse them.*/
+  static std::multimap<std::size_t, PMRBuffer> pmrBuffers;
+
+  /** Sizes of active pmr-memory allocations.  For convenience,
+      we keep track of the sizes of active allocations (i.e., those not
+      in the cache). */
+  static std::map<std::byte*, PMRState> pmrReservedBuffers;
+
+  static bool pmr_pool_init   = false;
+
+  /** whether to use a memory pool allocator for pinned memory */
+  static bool pmr_memory_pool = false;
+
+  //void init() { pmr_pool_init = true; }
+
+  template<bool is_exclusive = true>
+  decltype(auto) pmr_malloc_(const std::size_t nbytes, const bool is_reserved = false)
+  {
+    //
+    constexpr PRMState buffer_state = is_exclusive ? PMRState::Locked : PMRState::NonVacant;
+    
+    if ( pmrBuffers.empty() ) {
+      //
+      auto pmr_buffer = PMRBuffer{nbytes, is_reserved ? PMRState::Reserved : buffer_state};
+      //
+      pmrBuffers.insert(std::make_pair(nbytes, pmr_buffer));	
+      //
+      if ( is_reserved ) pmrReservedBuffers.insert(std::make_pair(pmr_buffer.Get(), buffer_state));
+        
+      return std::pmr::monotonic_buffer_resource{pmr_buffer.Get(), nbytes};
+        
+    } else {
+
+      auto range_begin = pmrBuffers.lower_bound(nbytes);
+      auto range_end   = pmrBuffers.end();
+          
+      if ( range_begin != range_end ) {
+        //
+        auto subrange_view = std::range::subrange(range_begin, range_end);
+	  
+        for(auto& [bytes, pmr_buffer] : subrange_view) {
+	  if (pmr_buffer.State() == PMRState::Vacant) {
+	     
+	    pmr_buffer.SetState( is_reserved ? PMRState::Reserved : buffer_state );
+	    //
+            if ( is_reserved ) pmrReservedBuffers.insert(std::make_pair(pmr_buffer.Get(), buffer_state));	      
+	    //
+	    return std::pmr::monotonic_buffer_resource{pmr_buffer.Get(), nbytes};
+	  } 
+        } 
+      }
+    }//
+    
+    return std::pmr::monotonic_buffer_resource{nbytes, upstream};//in all other cases?
+  }    
+
+    void pinned_free_(const char *func, const char *file, int line, void *ptr)
+    {
+      if (pinned_memory_pool) {
+        if (!pinnedSize.count(ptr)) { errorQuda("Attempt to free invalid pointer"); }
+        pinnedCache.insert(std::make_pair(pinnedSize[ptr], ptr));
+        pinnedSize.erase(ptr);
+      } else {
+        quda::host_free_(func, file, line, ptr);
+      }
+    }
+
+    void *device_malloc_(const char *func, const char *file, int line, size_t nbytes)
+    {
+      void *ptr = nullptr;
+      if (device_memory_pool) {
+        if (deviceCache.empty()) {
+          ptr = quda::device_malloc_(func, file, line, nbytes);
+        } else {
+          auto it = deviceCache.lower_bound(nbytes);
+          if (it != deviceCache.end()) { // sufficiently large allocation found
+            nbytes = it->first;
+            ptr = it->second;
+            deviceCache.erase(it);
+          } else { // sacrifice the smallest cached allocation
+            it = deviceCache.begin();
+            ptr = it->second;
+            deviceCache.erase(it);
+            quda::device_free_(func, file, line, ptr);
+            ptr = quda::device_malloc_(func, file, line, nbytes);
+          }
+        }
+        deviceSize[ptr] = nbytes;
+      } else {
+        ptr = quda::device_malloc_(func, file, line, nbytes);
+      }
+      return ptr;
+    }
+
+    void device_free_(const char *func, const char *file, int line, void *ptr)
+    {
+      if (device_memory_pool) {
+        if (!deviceSize.count(ptr)) { errorQuda("Attempt to free invalid pointer"); }
+        deviceCache.insert(std::make_pair(deviceSize[ptr], ptr));
+        deviceSize.erase(ptr);
+      } else {
+        quda::device_free_(func, file, line, ptr);
+      }
+    }
+
+#ifdef NVSHMEM_COMMS
+    void *shmem_malloc_(const char *func, const char *file, int line, size_t nbytes)
+    {
+      return quda::shmem_malloc_(func, file, line, nbytes);
+    }
+
+    void shmem_free_(const char *func, const char *file, int line, void *ptr)
+    {
+      quda::shmem_free_(func, file, line, ptr);
+    }
+#endif
+
+    void flush_pinned()
+    {
+      if (pinned_memory_pool) {
+        for (auto it : pinnedCache) { host_free(it.second); }
+        pinnedCache.clear();
+      }
+    }
+
+    void flush_device()
+    {
+      if (device_memory_pool) {
+        for (auto it : deviceCache) { device_free(it.second); }
+        deviceCache.clear();
+      }
+    }
+
+  } // namespace pool
+
 
 
 
