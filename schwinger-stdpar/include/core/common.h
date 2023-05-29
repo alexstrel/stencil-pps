796eb21d0251f4112926771ed1a8ebbd25569ae7
Strelchenko Alexei <astrel@fnal.gov>
2023-05-29 13:32:12 -0400

    refined container concepts

--- schwinger-stdpar/include/core/common.h
+++ schwinger-stdpar/include/core/common.h
@@ -54,39 +54,46 @@ concept ComplexTp = requires {
 template <typename T>
 concept ArithmeticTp = FloatTp<T> or ComplexTp<T>;
 
-// Generic container type:
-template <typename T, typename = std::void_t<>> class is_container_type : public std::false_type { };
+///////////////////////////////////////////////////////////////////////////
+template <typename T>
+concept GenericContainerTp = requires{
+    typename T::value_type;
+    typename T::size_type;
+    typename T::iterator;
 
-template <typename T> class is_container_type< T, std::void_t<decltype(std::declval<T>().begin()), 
-                                                              decltype(std::declval<T>().end()), 
-                                                              decltype(std::declval<T>().data()), 
-                                                              decltype(std::declval<T>().size())> > : public std::true_type { };
-                                                              
-template <typename T> constexpr bool is_container_type_v = is_container_type<T>::value;                                                              
+    { std::declval<T>().data()  }  -> std::same_as<typename T::value_type*>;
+    { std::declval<T>().size()  }  -> std::same_as<typename T::size_type>;
+    //
+    { std::declval<T>().begin() }  -> std::convertible_to<typename T::iterator>;
+    { std::declval<T>().end()   }  -> std::convertible_to<typename T::iterator>;
+}; 
+
+template<typename T>
+concept is_allocator_aware_type = requires{
+    typename T::allocator_type;
+    //requires std::same_as<decltype(std::declval<T>().get_allocator()), typename T::allocator_type>; //WHY this fails?..
+};
+
+template <typename T> constexpr bool is_allocated_type_v = is_allocator_aware_type<T>;
+
+//Allocator aware container type:
+template <typename T>
+concept ContainerTp = GenericContainerTp<T> and is_allocator_aware_type<T>;
+
+//Non-owning container type: 
+template <typename T>
+concept ContainerViewTp = GenericContainerTp<T> and (not is_allocator_aware_type<T>);
+
+//Polymorphic allocator aware container type:
 
 template <typename T>
-concept GenericContainerTp = is_container_type_v<T>; 
+concept is_pmr_allocator_aware_type = requires {
+    typename T::allocator_type;
+    requires std::same_as<typename T::allocator_type, std::pmr::polymorphic_allocator<typename T::value_type>>;
+};
 
-// More specific container type (allocator aware containers, e.g., std::vector etc.):
-//
-template <typename T, typename = std::void_t<>> class is_allocated_type : public std::false_type { };
-
-template <typename T> class is_allocated_type< T, std::void_t<typename T::allocator_type>> : public std::true_type { };
-
-template <typename T> constexpr bool is_allocated_type_v = is_allocated_type<T>::value;
-
-// Dynamic container type:
-template <typename T>
-concept ContainerTp = GenericContainerTp<T> and is_allocated_type_v<T>;
+template <typename T> constexpr bool is_pmr_allocator_aware_type_v = is_pmr_allocator_aware_type<T>;
 
 template <typename T>
-concept PMRContainerTp = ContainerTp<T> and std::is_same< typename T::allocator_type,  std::pmr::polymorphic_allocator<typename T::value_type> >::value;
-
-template <typename T>
-concept ContainerViewTp = GenericContainerTp<T> and (not is_allocated_type_v<T>);
-
-// Iterator type
-template <typename T>
-concept IteratorTp = std::random_access_iterator<T>;
-
+concept PMRContainerTp = GenericContainerTp<T> and is_pmr_allocator_aware_type<T>;
 
